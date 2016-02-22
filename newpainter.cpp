8188ff94e448a4e0a3b44afb1f651e52ce113cf8
Chena <leex8119@umn.edu>
2016-02-21 19:04:20 -0600

    added newpainter.cpp

---
+++ newpainter.cpp
@@ -0,0 +1,17 @@
+//This program is to alter processed image to ptg.
+
+#include <iostream>
+using namespace std;
+int main(){
+	//read the file and make sure the file is open.
+	//put image into source image
+	//int rows, int columns, int original[rows][columns];
+	//int altered[rows][columns];
+	//output txt.
+	
+	//findConsecutivePosition(vector, color, original)
+	//output vector
+	
+
+	return 0;
+}
