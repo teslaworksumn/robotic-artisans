fa247a4fda32451cd9cf09840fe9b1fcfb18e13d
Brooke Padilla <padil031@umn.edu>
2016-02-22 14:14:26 -0600

    I added some of the variables we will need.  I will also want you to complete the initialize_original

--- newpainter.cpp
+++ newpainter.cpp
@@ -1,8 +1,33 @@
 //This program is to alter processed image to ptg.
-
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <istream>
 using namespace std;
+
+int initialize_original();
+
+
+
+
+
+
+
+
+
+
 int main(){
+	//declare variables
+	int row;
+	int col;
+	int **original_img;
+	int **final_img;
+	string iFileName;
+	ifstream iFile;
+	string oFileName;
+	ofstream oFile;
+	vector<int[2]> patch;//patch is the color patch
 	//read the file and make sure the file is open.
 	//put image into source image
 	//int rows, int columns, int original[rows][columns];
@@ -15,3 +40,15 @@ int main(){
 
 	return 0;
 }
+
+//This will take in the file and the original image pointer 
+//Will take the istream and the original_img array, 
+//the row # and the column #
+//return 0 if all of original_img is filled
+//return -1 if problem with reading in img
+int initialize_original(istream &iFile, int ** original_img, int row, int col)
+{
+
+	return 0; //SUCCESS
+}
+
