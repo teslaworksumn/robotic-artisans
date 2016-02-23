79b18b4d354b7f01eb1f290e5336b35763d63fee
Chena <leex8119@umn.edu>
2016-02-22 18:48:11 -0600

    checking errors

--- newpainter.cpp
+++ newpainter.cpp
@@ -30,6 +30,19 @@ int main(){
 	ofstream oFile;
 	vector<int[2]> patch;//patch is the color patch
 	int a[5][3];
+
+	cout<<"Input the original image file: ";
+	cin>>iFileName;
+	cout<<"Input the desired output file name:";
+	cin>>oFileName;
+	
+	iFile.open(iFileName); //read the file and make sure the file is open.
+	if(iFile.fail()){
+		cout<<"Input file opening failed."<<endl;
+		return -1;
+	}
+
+
 	/*
 	for(int i=0; i<5; i++){
 		for(int j=0; j<3;j++){
@@ -40,7 +53,6 @@ int main(){
 	}
 	*/
 	
-	//read the file and make sure the file is open.
 	//put image into source image
 	//int rows, int columns, int original[rows][columns];
 	//int altered[rows][columns];
@@ -58,13 +70,18 @@ int main(){
 //the row # and the column #
 //return 0 if all of original_img is filled
 //return -1 if problem with reading in img
-int initialize_original(istream &iFile, int ** original_img, int row, int col)
+int initialize_original(ifstream &iFile, int ** original_img, int row, int col)
 {
-
+	for(int i=0, i<=row, i++){
+		for(int j=0, j<=col, i++){
+			iFile>>original_img[i][j];
+		}
+	}
 	return 0; //SUCCESS
 }
 
 void initialize_final( int ** final_img, int row, int col ){
 	
+
 }
 
