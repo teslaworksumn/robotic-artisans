7497993342bf9d029ec0c0949848dae07f013410
Elliott Beach <elliott2.71828@gmail.com>
2016-11-05 09:18:55 -0500

    can read flags

--- pathPlanners/painter/path_planner.py
+++ pathPlanners/painter/path_planner.py
@@ -9,25 +9,39 @@ import sys
 # default input/output files
 iFileName = "../../ptg_pictures/lisa.ptg"
 oFileName = "lisa.py.txt"
-
+debug = False
 
 def USAGE_STATEMENT():
 	print("Oops!")
 
 def set_flags(args):
+	global iFileName
+	global oFileName
+	global debug
+	print args
 	changedOf = False
+	args = args[1:]
 	for arg in args:
 		if arg == "-h":
 			USAGE_STATEMENT()
-			return false
+			return False
 		elif arg[0:3] == "-ci":
 			iFileName = arg[3:]
 			if not changedOf:
 				# make input file have same encoding as output file, but replace ptg with txt
-				assert iFileName[:3] == "ptg"
-				oFileName = iFileName[:3] + "txt"
-
-
+				assert iFileName[-4:] == ".ptg"
+				oFileName = iFileName[-4:] + ".txt"
+		elif arg[0:3] == "-co":
+			changedOf = True
+			oFileName = arg[2:]
+		elif arg == "-d":
+			debug = True
+		else:
+			print ("ERROR: UNKNOWN ARGUMENT")
+			print (arg)
+			USAGE_STATEMENT()
+			return False
+	return True
 
 
 def main():
