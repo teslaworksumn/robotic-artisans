a648f962385cd1a8135ce581320833dde0b008f5
Elliott Beach <elliott2.71828@gmail.com>
2016-11-05 13:46:34 -0500

    finish set flags function

--- pathPlanners/painter/path_planner.py
+++ pathPlanners/painter/path_planner.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 """Translate a file from ptg format to paint instruction format.
 
 arguments:
@@ -11,28 +13,36 @@ iFileName = "../../ptg_pictures/lisa.ptg"
 oFileName = "lisa.py.txt"
 debug = False
 
-def USAGE_STATEMENT():
-	print("Oops!")
+def USAGE_STATEMENT(filename):
+	message = """
+	Usage:\t\tpython path_planner.py [options]
+	Options:
+	-h \t\tPrint this message and exit
+	-ci\t\tWill ask for input file other than default
+	-co\t\tWill ask for output file other than default
+	-d \t\tWill turn on debug mode
+	"""
+	print (message)
 
+	
 def set_flags(args):
 	global iFileName
 	global oFileName
 	global debug
-	print args
-	changedOf = False
+	changeOFileName = False
 	args = args[1:]
 	for arg in args:
 		if arg == "-h":
-			USAGE_STATEMENT()
+			USAGE_STATEMENT("foo")
 			return False
 		elif arg[0:3] == "-ci":
 			iFileName = arg[3:]
-			if not changedOf:
+			if not changeOFileName:
 				# make input file have same encoding as output file, but replace ptg with txt
 				assert iFileName[-4:] == ".ptg"
 				oFileName = iFileName[-4:] + ".txt"
 		elif arg[0:3] == "-co":
-			changedOf = True
+			changeOFileName = True
 			oFileName = arg[2:]
 		elif arg == "-d":
 			debug = True
