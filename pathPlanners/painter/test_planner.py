dfce02472a058108d0009d832908c20608da596f
Elliott Beach <elliott2.71828@gmail.com>
2016-11-06 00:41:22 -0500

    rename tests

--- pathPlanners/painter/test_planner.py
+++ pathPlanners/painter/test_planner.py
@@ -1,10 +1,76 @@
 import path_planner
+import pytest
 
-def test_open_file():
-	"check how python throws exceptiosn when opening files"
+def test_open_file_test():
+	"""test how python works"""
 	try:
-		with open("non_existant_file") as foo:
+		with open("__non_existent_file__") as foo:
 			assert False
-	except:
+	except IOError:
 		assert True
+
+
+def test_debug():
+	options = "_ -d".split()
+	_, _, debug = path_planner.set_flags(options)
+	assert debug
+	options = "_"
+	_, _, debug = path_planner.set_flags(options)
+	assert not debug
+
+def test_input_file():
+	options = "_ -ci=EXAMPLE".split()
+	input_file, _, _ = path_planner.set_flags(options)
+	assert input_file == "EXAMPLE"
+
+def test_output_file():
+	options = "_ -co=EXAMPLE".split()
+	_, output_file, _ = path_planner.set_flags(options)
+	assert output_file == "EXAMPLE"
+
+
+def test_sample_file_exists():
+	""" this file needs to not move """
+	open("../../ptg_pictures/lisa.ptg").close()
+
+@pytest.fixture
+def sample_input():
+	""" sample contents of input file """
+	file = open("../../ptg_pictures/lisa.ptg")
+	yield file
+	# teardown
+	file.close()
+
+@pytest.fixture
+def bad_input():
+	""" non-ptg format file """
+	file = open("test_planner.py")
+	yield file
+	# teardown
+	file.close()
+
+def test_read_numbers(sample_input):
+	matrix = path_planner.read_numbers(sample_input)
+	assert matrix == [
+		[3, 4],
+		[1, 2, 3, 1],
+		[3, 1, 1, 2],
+		[2, 2, 1, 3]
+	]	
+
+def test_read_numbers(bad_input):
+	""" should exit on invalid file name """
+	try:
+		matrix = path_planner.try_to_read_numbers(bad_input)
+	except SystemExit:
+		pass
+	else:
+		assert False
+
+
+
+
+
+
+	
 
