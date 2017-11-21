918d3bc32e1d6f848aa8d11e0a834e922f6d27d6
Max Omdal <mjomdal@gmail.com>
2017-11-21 14:56:17 -0600

    error handles bad images

--- dataset_processor/imageprocessor.py
+++ dataset_processor/imageprocessor.py
@@ -15,12 +15,18 @@ def batch_resize(folders, w, h):
         if not os.path.exists(resized_img_path):
             os.makedirs(resized_img_path)
         for item in dirs:
-            if os.path.isfile(path + item) and item.:
-                im = Image.open(path + item)
-                imResize = im.resize((w,h), Image.ANTIALIAS)
-                filename = os.path.basename(path + item)
-                filename, extension = os.path.splitext(filename)
-                imResize.save(resized_img_path + filename + '_resized.png', 'png')
+            if os.path.isfile(path + item):
+                f, e = os.path.splitext(item)
+                # make sure there only te right file types are used
+                if (e == 'png' or e == 'jpg'):
+                    # create a PIL Image with the file
+                    im = Image.open(path + item)
+                    # resize
+                    imResize = im.resize((w,h), Image.ANTIALIAS)
+                    # save
+                    filename = os.path.basename(path + item)
+                    filename, extension = os.path.splitext(filename)
+                    imResize.save(resized_img_path + filename + '_resized.png', 'png')
 
 # creates a file with all the pixels of each image written out with ' ' delimiter
 # images parameter is an array of PIL Images
@@ -33,6 +39,7 @@ def createDataset(images):
 
     np.random.shuffle(random_train)
 
+    # write out files for images
     with open('train/images_train.csv', 'w+') as a, open('train/images_test.csv', 'w+') as b:
         for index in range(len(images)):
             image_index = random_train[index]
@@ -41,19 +48,20 @@ def createDataset(images):
             pixels_string = ','.join(map(str, pixels))
             classification = SHAPE_LABELS[image[0]]
             if (index < len(images)*3/4):
-                print(pixels_string, file = a)
+                a.write(pixels_string + '\n')
             else:
-                print(pixels_string, file = b)
+                b.write(pixels_string + '\n')
 
+    # write out files for labels
     with open('train/labels_train.csv', 'w+') as a, open('train/labels_test.csv', 'w+') as b:
         for index in range(len(images)):
             image_index = random_train[index]
             image = images[image_index]
             classification = SHAPE_LABELS[image[0]]
             if (index < len(images)*3/4):
-                print(','.join(repr(e) for e in classification), file = a)
+                a.write(','.join(repr(e) for e in classification) + '\n')
             else:
-                print(','.join(repr(e) for e in classification), file = b)
+                b.write(','.join(repr(e) for e in classification) + '\n')
 
 # make a PIL Image instance for each image and return a list of images
 def createImages(folders):
@@ -67,9 +75,11 @@ def createImages(folders):
                 images = images + [(folder, new_image)]
     return images
 
+
 if __name__ == '__main__':
     # create a list of file names where the images are stored
     folders = ['triangle', 'circle', 'square']
 
+    # create new directories of images (500, 500) for the given directories of images
     batch_resize(folders, 500, 500)
     createDataset(createImages(folders))
