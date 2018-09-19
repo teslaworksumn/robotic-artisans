f090375ef3ace966acfe43c7b0acedbbf001d072
Max Omdal <mjomdal@gmail.com>
2018-09-18 21:17:40 -0500

    work on edge generator

--- edge-path-maker/make-gcode.py
+++ edge-path-maker/make-gcode.py
@@ -1,20 +1,28 @@
 import Edge_Detection
 
+scale_factor = 10
+
 def makeGCode(edges, width, height):
     commands = []
     oldpoint = (0,0)
-    commands += ["G90 (Set to absolute positioning)\n"]
     commands += ["G21 (set to mm units)\n"]
-    commands += ["$22=1 $23=2(home)\n"] # this probably needs to be fixed
+    #commands += ["$22=1 $23=2(home)\n"] # this probably needs to be fixed
     for edge in edges:
-        commands += ["G1 " + "X" + str(edge[0][0]) + " " + "Y" + str(edge[0][1]) + " Z0 F100"]
-        commands += ["G1 X0 Y0 Z5"]
-        for point in edge:
+        #commands += ["G1 X0 Y0 Z5"]
+        i = 0
+        for i in range(0, len(edge) - 2, 2):
+            point = edge[i]
             x = point[0]# - oldpoint[0]
             y = point[1]# - oldpoint[1]
-            commands += ["G1 " + "X" + str(x) + " " + "Y" + str(y) + " Z0 F100"]
+            commands += ["G91 G0 " + "X" + str((x - oldpoint[0])/scale_factor) + " " + "Y" + str((y - oldpoint[1])/scale_factor)]
             oldpoint = point
-        commands += ["G1 X0 Y0 Z50"]
+        #commands += ["G1 X0 Y0 Z5"]
+        for i in range(len(edge)-2, len(edge)):
+            point = edge[i]
+            x = point[0]# - oldpoint[0]
+            y = point[1]# - oldpoint[1]
+            commands += ["G91 G0 " + "X" + str((x - oldpoint[0])/scale_factor) + " " + "Y" + str((y - oldpoint[1])/scale_factor)]
+            oldpoint = point
 
     return commands
 
